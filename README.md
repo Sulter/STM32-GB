cf9ab5fc5640739b609870af95158e28912288bb
Sulter <Sulter@users.noreply.github.com>
2014-12-08 22:29:22 +0100

    License the project, adding travis support and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# STM32-GB [![Build Status](https://travis-ci.org/Sulter/STM32-GB.svg?branch=master)](https://travis-ci.org/Sulter/STM32-GB)
+========
+
+A Game Boy emulator that will hopefully run on a STM32 one day.
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,13 @@
 #include "../include/Cpu.h"
+#include "../include/Memory.h"
 #include <iostream>
 #include <cstdint>
 #include <cstdio>
 
 int main()
 {
-    uint8_t a,b;
-    a= 200;
-    b= 57;
-    //detect overflow
-    if(a+b>255)
-	printf("overflow\n");
-    if(a-b<0)
-	printf("underflow\n");
-
-
-    Cpu myCpu;
+    Memory myMMU;
+    Cpu myCpu(myMMU);
+    myCpu.incA();
     return 0;
 }
