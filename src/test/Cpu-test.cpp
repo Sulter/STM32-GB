ec66158c6d280cff12e1111556772be20c83941a
Sulter <Sulter@users.noreply.github.com>
2019-09-11 21:31:26 +0200

    Add first unit test

---
+++ src/test/Cpu-test.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "../../include/Cpu.h"
+
+// The fixture for testing class Foo.
+class CpuTest : public ::testing::Test
+{
+ protected:
+  CpuTest() {
+  }
+
+  ~CpuTest() override {
+  }
+
+  void SetUp() override {
+  }
+
+  void TearDown() override {
+  }
+
+
+  Cpu cpu;
+};
+
+TEST_F(CpuTest, instruction1test) 
+{
+
+}
