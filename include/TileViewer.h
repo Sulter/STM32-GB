469ea326f70069a9d95b3104874495568061dc61
Sulter <Sulter@users.noreply.github.com>
2019-09-30 19:49:26 +0200

    Add tile editing to TileViewer

--- include/TileViewer.h
+++ include/TileViewer.h
@@ -2,13 +2,14 @@
 #include <imgui.h>
 #include <string>
 #include <bitset>
+#include <cmath>
 
 class TileViewer
 {
 public:
   void DrawWindow(std::string name, uint8_t *memPtr)
   {
-    ImGui::Begin(name.c_str(), nullptr, ImVec2(pixelSize * 8 + 150, pixelSize * 8 + 100), 1.0f, ImGuiWindowFlags_NoResize);
+    ImGui::Begin(name.c_str(), nullptr, ImVec2(pixelSize * tileSize + xOffset, pixelSize * tileSize + yOffset), 1.0f, ImGuiWindowFlags_NoResize);
     ImGui::SliderInt("size", &pixelSize, 1, 30);
     ImGuiInputTextFlags flags = ImGuiInputTextFlags_CharsHexadecimal | ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_NoHorizontalScroll | ImGuiInputTextFlags_AlwaysInsertMode;
     if (ImGui::InputText("addr", inputBuf, 5, flags))
@@ -17,14 +18,13 @@ public:
     }
 
     if (!hasResized)
-      ImGui::SetWindowSize(ImVec2(pixelSize * 8 + 150, pixelSize * 8 + 100));
+      ImGui::SetWindowSize(ImVec2(pixelSize * tileSize + xOffset, pixelSize * tileSize + yOffset));
+
     ImDrawList *draw_list = ImGui::GetWindowDrawList();
     ImVec2 p = ImGui::GetCursorScreenPos();
 
     for (uint16_t line = 0; line < 8; line++)
     {
-      //volatile uint16_t tileIdAddr = startAddrTiles; //startAddr + tileY + tileX;
-      //volatile uint8_t tileId = MMU.readByte(tileIdAddr);
       std::bitset<8> tileLine1 = memPtr[tileAddr + 2 * line];
       std::bitset<8> tileLine2 = memPtr[tileAddr + 2 * line + 1];
       tileLine1 |= tileLine2;
@@ -38,10 +38,52 @@ public:
       }
     }
 
+    if (ImGui::IsMousePosValid())
+    {
+      ImGuiIO &io = ImGui::GetIO();
+
+      if (ImGui::IsMouseClicked(0))
+      {
+        float x = io.MousePos.x;
+        float y = io.MousePos.y;
+        x = x - p.x - pixelSize;
+        y = y - p.y;
+
+        //check if mouse inside drawing area
+        if (x >= 0 && x <= pixelSize * 8 && y >= 0 && y <= pixelSize * 8)
+        {
+          int tilePxX = static_cast<int>(round(x)) / (pixelSize);
+          int tilePxY = static_cast<int>(round(y)) / (pixelSize);
+
+          changeTilePixel(tilePxX, tilePxY, memPtr);
+        }
+      }
+    }
+
     ImGui::End();
   };
 
 private:
+  void changeTilePixel(int tilePxX, int tilePxY, uint8_t *memPtr)
+  {
+    if (tilePxY < 0 && tilePxY > 7 && tilePxX < 0 && tilePxX > 7)
+      return;
+
+    //because of lsb
+    tilePxX = 7 - tilePxX;
+
+    if ((memPtr[tileAddr + 2 * tilePxY] & (1 << tilePxX)) | (memPtr[tileAddr + 2 * tilePxY + 1] & (1 << tilePxX)))
+    {
+      memPtr[tileAddr + 2 * tilePxY] &= ~(1 << tilePxX);
+      memPtr[tileAddr + 2 * tilePxY + 1] &= ~(1 << tilePxX);
+    }
+    else
+    {
+      memPtr[tileAddr + 2 * tilePxY] |= 1 << tilePxX;
+      memPtr[tileAddr + 2 * tilePxY + 1] |= 1 << tilePxX;
+    }
+  }
+
   int pixelSize = 5;
   bool hasResized = false;
 
@@ -52,4 +94,8 @@ private:
 
   uint16_t tileAddr = 0x8205; //R logo
   char inputBuf[5] = "8205";
+
+  static constexpr int xOffset = 150;
+  static constexpr int yOffset = 100;
+  static constexpr int tileSize = 8;
 };
